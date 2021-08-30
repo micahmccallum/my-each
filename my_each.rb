027378b67fc05f76e53bffa9ca9c368f858dc6d0
Micah <micahmccallum@outlook.com>
2021-08-30 16:18:53 -0500

    commit tree f5e5eb5e1edc5181e05ed1f817a459455a9c0130

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,8 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  i = 0
+  while collection.length - i > 0
+    yield(collection[i])
+    i += 1
+  end
+  collection
 end
